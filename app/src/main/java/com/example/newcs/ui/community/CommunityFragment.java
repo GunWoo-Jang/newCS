b3e457b34b3965cfe461ef390d43e20a0b363c24
Gunjang <jgy5628@naver.com>
2020-10-13 19:17:26 +0900

    ver1.03

--- app/src/main/java/com/example/newcs/ui/community/CommunityFragment.java
+++ app/src/main/java/com/example/newcs/ui/community/CommunityFragment.java
@@ -25,7 +25,7 @@ public class CommunityFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.community_fragment, container, false);
+        return inflater.inflate(R.layout.fragment_community, container, false);
     }
 
     @Override
